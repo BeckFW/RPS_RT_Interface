3460904a9bca6ced7498634210f0b02e0efb2024
BeckFW <beck@elbo.me>
2024-03-21 16:41:17 +0000

    Main.js comments structure

---
+++ main.js
@@ -0,0 +1,15 @@
+// Setup 
+
+// Play welcome message
+
+// TODO: Find a way to start the game
+
+// Start Game
+
+// Play countdown sequence
+
+// Determine result
+
+// Play result sequence
+
+// Play scores sequence/CF
